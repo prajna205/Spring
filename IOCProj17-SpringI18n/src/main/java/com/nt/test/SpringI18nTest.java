0ba77103f197645a03596437f3c3a990ff66f50b
Prajna <56439109+prajna205@users.noreply.github.com>
2023-03-19 13:25:49 +0530

    internationalization

---
+++ IOCProj17-SpringI18n/src/main/java/com/nt/test/SpringI18nTest.java
@@ -0,0 +1,21 @@
+package com.nt.test;
+
+import java.util.Locale;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringI18nTest {
+
+	public static void main(String[] args) {
+		// create AC container
+		ClassPathXmlApplicationContext ctx= new ClassPathXmlApplicationContext("com/nt/cfgs/applicationContext.xml");
+		//get message from properties file according to locale we submit
+		Locale locale = new Locale(args[0], args[1]);
+		String msg1= ctx.getMessage("wish.msg",new String[] {}, locale);
+		String msg2= ctx.getMessage("bye.msg",new String[] {}, locale);
+//print the messages
+		System.out.println(msg1);
+		System.out.println(msg2);
+	}
+
+}
