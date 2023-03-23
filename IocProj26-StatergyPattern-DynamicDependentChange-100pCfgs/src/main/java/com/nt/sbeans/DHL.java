84dd2fcaf4fc6cbba48eb701375048fb8787213a
Prajna <56439109+prajna205@users.noreply.github.com>
2023-03-23 09:07:01 +0530

    dynamic dependent change

---
+++ IocProj26-StatergyPattern-DynamicDependentChange-100pCfgs/src/main/java/com/nt/sbeans/DHL.java
@@ -0,0 +1,18 @@
+//Dependent class4
+package com.nt.sbeans;
+
+import org.springframework.stereotype.Component;
+
+@Component("dhl")
+public final class DHL implements Courier{   // rule3 ,rule2
+	
+	public DHL() {
+		System.out.println("DHL:: 0-param constructor");
+	}
+	
+	@Override
+	public String deliver(int oid) {
+		return  oid+ "order id oder is assinged to DHL for Delivery";
+	}
+
+}
